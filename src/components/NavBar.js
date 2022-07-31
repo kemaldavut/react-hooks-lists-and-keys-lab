8ad5d666e72eae2fbb46a0cba6c2b52dcd2cd254
Kemal DAVUT <mustafakemaldavut@gmail.com>
2022-07-31 14:56:08 +0300

    Completed assignment

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,7 +3,11 @@ import React from "react";
 function NavBar() {
   const links = ["home", "about", "projects"];
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  const navbarItems = links.map((link, index) => {
+    return <a key={index} href={"#"+link}>{link}</a>;
+  })
+  return <nav>{navbarItems}</nav>;
+
 }
 
 export default NavBar;
